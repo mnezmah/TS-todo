1ea40a426b62b5d0f719538e4068be8c1eb92ce4
mnezmah <mario.nezmah@gmail.com>
2019-08-08 18:26:46 +0200

    Add Pages

---
+++ src/pages/controller.ts
@@ -0,0 +1,13 @@
+import { JsonController, Get, Param } from 'routing-controllers'
+import pagesById, { Page } from './data'
+
+@JsonController()
+export default class PageController {
+
+  @Get('/pages/:id')
+  getPage(
+    @Param('id') id: number
+  ): Page {
+    return pagesById[id]
+  }
+}
